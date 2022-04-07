a4c6c154750cad50e81a3be30bef47866ef92278
Wagner Carvalho <wagner.carvalho@credisis.com.br>
2022-04-07 16:12:25 -0300

    totalizando pontuacao, mensagem resultado

--- lib/main.dart
+++ lib/main.dart
@@ -68,7 +68,7 @@ class _PerguntaAppState extends State<PerguntaApp> {
                 perguntaSelecionada: _perguntaSelecionada,
                 quandoResponder: _responder,
               )
-            : Resultado(),
+            : Resultado(_pontuacaoTotal),
       ),
       // )
     );
